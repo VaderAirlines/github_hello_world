d53411ef7016d0ed06e6bbbd3eb750380f7ab66c
VaderAirlines <dennisbellinkx@gmail.com>
2015-07-25 20:59:30 +0200

    first commit

---
+++ github_hello_world.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace github_hello_world.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
